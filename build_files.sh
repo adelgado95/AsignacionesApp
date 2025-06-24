e6c6b8d27cc2ebc36def66f29e69074f2b9b03ae
Alfonso Delgado <alfonsofuentes@reps2.com>
2025-06-23 18:28:46 -0600

    trygin deploy

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3 manage.py makemigrations --no-input
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input --clear
