9f23dc8cdf2a801757e184f2e46c5d092fc568ff
Alfonso Delgado <alfonsofuentes@reps2.com>
2025-08-11 14:44:32 -0600

    Fixinf vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 
 pip3 install -r requirements.txt
-python3 manage.py makemigrations --no-input
-python3 manage.py migrate --no-input
+python3 -V
+#python3 manage.py makemigrations --no-input
+#python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input --clear
 
