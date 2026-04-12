4486c86dd38cc46b25bcce23922fbda0b4bd3012
Alfonso Delgado <ajdfdelgado@gmail.com>
2026-04-12 16:25:56 -0600

    add sql file for split apps

--- app/settings.py
+++ app/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / os.getenv('SQL_FILE', 'db.sqlite3'),
     }
 }
 
